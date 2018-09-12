3c39c12ca32af85b31bbbc4d7381faba3b547e95
wsq5477 <492696846@qq.com>
2018-09-12 10:22:06 +0800

    city-search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,15 +1,18 @@
 <template>
 	<div>
 		<city-header></city-header>
+		<city-search></city-search>
 	</div>
 </template>
 <script>
 import cityHeader from './components/Header.vue'
+import citySearch from './components/Search.vue'
 
 export default{
 	name:"City",
 	components:{
 		cityHeader,
+		citySearch
 	}
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="city-search">
+  	  <input type="text" name="城市搜索" placeholder="输入城市名或名称" class="search-input">
+  </div>
+</template>
+<script>
+export default{
+	name:"citySearch"
+}
+</script>
+<style lang="stylus">
+   .city-search{
+      background:#00bcd4;
+      height:.72rem;
+      padding:0 .1rem;
+      .search-input{
+        box-sizing:border-box;
+         text-align:center;
+         width:100%;
+         height:.62rem;
+         font-size:.1rem;
+         color:#666;
+         padding:0 .1rem;
+         border-radius:.06rem;
+      }
+   }
+</style>
